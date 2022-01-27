ce70081fd0ed550676e80510c006b5d279678519
Samuele Longhin <longhin.samuele@outlook.com>
2022-01-27 08:27:13 +0100

    aggiunta form-data*-formats

--- src/Formatter.php
+++ src/Formatter.php
@@ -2,48 +2,84 @@
 
 namespace samuelelonghin\formatter;
 
+use yii\base\InvalidConfigException;
+
 class Formatter extends \yii\i18n\Formatter
 {
-    public $dbDateTimeFormat = 'yyyy-MM-dd HH:mm:ss';
-    public $dbDateFormat = 'yyyy-MM-dd';
-    public $dbTimeFormat = 'HH:mm:ss';
+	public $dbDateTimeFormat = 'yyyy-MM-dd HH:mm:ss';
+	public $dbDateFormat = 'yyyy-MM-dd';
+	public $dbTimeFormat = 'HH:mm:ss';
 
-    /**
-     * @param $value
-     * @return string
-     * @throws \yii\base\InvalidConfigException
-     */
-    public function asDbDateTime($value)
-    {
-        return $this->asDateTime($value, $this->dbDateTimeFormat);
-    }
+	public $formDateTimeFormat;
+	public $formDateFormat;
+	public $formTimeFormat;
 
-    /**
-     * @param $value
-     * @return string
-     * @throws \yii\base\InvalidConfigException
-     */
-    public function asDbDate($value)
-    {
-        return $this->asDate($value, $this->dbDateFormat);
-    }
+	/**
+	 * @param $value
+	 * @return string
+	 * @throws InvalidConfigException
+	 */
+	public function asDbDateTime($value)
+	{
+		return $this->asDateTime($value, $this->dbDateTimeFormat);
+	}
 
-    /**
-     * @param $value
-     * @return string
-     * @throws \yii\base\InvalidConfigException
-     */
-    public function asDbTime($value)
-    {
-        return $this->asTime($value, $this->dbTimeFormat);
-    }
+	/**
+	 * @param $value
+	 * @return string
+	 * @throws InvalidConfigException
+	 */
+	public function asFormDateTime($value)
+	{
+		return $this->asDateTime($value, $this->formDateTimeFormat);
+	}
 
-    /**
-     * @param $value
-     * @return string
-     */
-    public function asNumber($value): string
-    {
-        return $this->asText($value);
-    }
+	/**
+	 * @param $value
+	 * @return string
+	 * @throws InvalidConfigException
+	 */
+	public function asDbDate($value)
+	{
+		return $this->asDate($value, $this->dbDateFormat);
+	}
+
+	/**
+	 * @param $value
+	 * @return string
+	 * @throws InvalidConfigException
+	 */
+	public function asFormDate($value)
+	{
+		return $this->asDate($value, $this->formDateFormat);
+	}
+
+	/**
+	 * @param $value
+	 * @return string
+	 * @throws InvalidConfigException
+	 */
+	public function asDbTime($value)
+	{
+		return $this->asTime($value, $this->dbTimeFormat);
+	}
+
+	/**
+	 * @param $value
+	 * @return string
+	 * @throws InvalidConfigException
+	 */
+	public function asFormTime($value)
+	{
+		return $this->asTime($value, $this->formTimeFormat);
+	}
+
+	/**
+	 * @param $value
+	 * @return string
+	 */
+	public function asNumber($value): string
+	{
+		return $this->asText($value);
+	}
 }
