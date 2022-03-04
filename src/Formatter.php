c67a21c7626c7d3ba1bf0d44c41afbc09eed4d61
Samuele Longhin <longhin.samuele@outlook.com>
2022-03-04 09:34:48 +0100

    Aggiunti un po' di tipi

--- src/Formatter.php
+++ src/Formatter.php
@@ -2,7 +2,13 @@
 
 namespace samuelelonghin\formatter;
 
+use app\models\ModelString;
+use samuelelonghin\db\ActiveRecord;
+use Yii;
+use yii\base\Exception;
 use yii\base\InvalidConfigException;
+use yii\bootstrap4\Html;
+use function Symfony\Component\String\s;
 
 class Formatter extends \yii\i18n\Formatter
 {
@@ -88,6 +94,26 @@ class Formatter extends \yii\i18n\Formatter
 	 * @return string
 	 * @throws InvalidConfigException
 	 */
+	public function asRawTime($value)
+	{
+		$temp = $this->timeZone;
+		$this->timeZone = $this->defaultTimeZone;
+		$res = $this->asTime($value, $this->dbTimeFormat);
+		$this->timeZone = $temp;
+
+		return $res;
+	}
+
+	public function asMinutes($value)
+	{
+		return $this->asRawTime(intval($value) * 60);
+	}
+
+	/**
+	 * @param $value
+	 * @return string
+	 * @throws InvalidConfigException
+	 */
 	public function asFormTime($value)
 	{
 		return $this->asTime($value, $this->formTimeFormat);
@@ -100,5 +126,44 @@ class Formatter extends \yii\i18n\Formatter
 	public function asNumber($value): string
 	{
 		return $this->asText($value);
+	}
+
+
+	public function asScadenzaMesi($value): string
+	{
+		$mesi = intval($value);
+		return $this->asText(
+			Yii::t('app/formatter', '{n,plural,=0{non ha scadenza} =1{1 mese} other{# mesi}}', ['n' => $mesi])
+		);
+	}
+
+	public function asScadenza($value): string
+	{
+		if (($mesi = intval($value)))
+			throw new Exception('cambia in ScadenzaMesi');
+		return $this->asText(
+			Yii::t('app/formatter', '{n,plural,=0{non ha scadenza} =1{1 mese} other{# mesi}}', ['n' => $mesi])
+		);
+	}
+
+	public function asDataScadenza($value): string
+	{
+		if (!$value || $value < 0) {
+			return Yii::t('app/formatter', 'mai');
+		}
+		return $this->asDate($value);
+	}
+
+	/**
+	 * @param $value
+	 * @return string|null
+	 */
+	public function asLink($value): ?string
+	{
+		if ($value instanceof ModelString && $value instanceof ActiveRecord) {
+			$text = $this->asText($value->toString());
+			return Html::a($text, ['/' . $value::getController() . '/view', 'id' => $value->id]);
+		}
+		return null;
 	}
 }
